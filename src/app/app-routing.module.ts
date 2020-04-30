65d81a8c2fa27045ffd63759345384bd0549460d
Dmitry <vasilev.dm@gmail.com>
2020-04-30 10:25:29 +0300

    add login routing, move auth routing to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {AuthComponent} from './auth/auth.component';
+import {WelcomeComponent} from './welcome/welcome.component';
 
 const routes: Routes = [
-  { path: '', component: AuthComponent }
+  { path: '', component: WelcomeComponent }
 ];
 
 @NgModule({
